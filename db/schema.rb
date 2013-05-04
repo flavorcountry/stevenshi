efa1390b705cc3a3e2ad1449bc7726dc62b315ca
Steven Shi <steven.shi5294@gmail.com>
2013-05-04 11:06:01 +0800

    Category show and slug

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,13 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130501015900) do
+ActiveRecord::Schema.define(:version => 20130504030221) do
 
   create_table "categories", :force => true do |t|
     t.string   "name"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.string   "slug"
   end
 
   create_table "portfolios", :force => true do |t|
