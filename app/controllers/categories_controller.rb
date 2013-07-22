44b3e47af06d04084aed057b0a013b9fc73ca3d7
Steven Shi <steven.shi5294@gmail.com>
2013-07-22 15:20:14 +0800

    Reorganize category posts by date

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,6 +2,7 @@ class CategoriesController < ApplicationController
 
 	def show
 		@category = Category.find(params[:id])
+		@posts = @category.posts.order("created_at desc").page(params[:page]).per_page(5)
 	end
 
 end
