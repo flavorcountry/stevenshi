1812f1b1e1a6cc2de782ea67d0bc6ad73f8b5214
Steven Shi <steven.shi5294@gmail.com>
2013-07-22 17:39:47 +0800

    delete function for posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,24 +1,24 @@
 class PostsController < ApplicationController
   def index
-  	@posts = Post.order("created_at desc").page(params[:page]).per_page(5)
+    @posts = Post.order("created_at desc").page(params[:page]).per_page(5)
   end
 
   def show
-  	@post = Post.find(params[:id])
+    @post = Post.find(params[:id])
   end
 
   def new
-  	@post = Post.new
+    @post = Post.new
   end
 
   def create
-  	password = params[:post].delete('password')
-  	if User.first.authenticate(password)
-  		@post = Post.create(params[:post])
+    password = params[:post].delete('password')
+    if User.first.authenticate(password)
+      @post = Post.create(params[:post])
       redirect_to root_path
-  	else
-  		redirect_to root_path, error: "Nope."
-  	end
+    else
+      redirect_to root_path, error: "Nope."
+    end
   end
 
   def edit
@@ -35,4 +35,21 @@ class PostsController < ApplicationController
       redirect_to post_path(@post), error: "Nope."
     end
   end
+
+  def destroy_confirmation
+    @post = Post.find(params[:id])
+  end
+
+  def destroy
+    password = params[:password]
+    @post = Post.find(params[:id])
+
+    if User.first.authenticate(password)
+      @post.delete
+      redirect_to root_path
+    else
+      redirect_to destroy_confirmation_path(id: @post.id)
+    end
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Postit::Application.routes.draw do
   root to: 'posts#index'
   get '/about', to: "static#about"
   get '/archive', to: 'static#archive'
+  get '/destroy_confirmation', to: "posts#destroy_confirmation"
 
   resources :posts
   resources :categories
